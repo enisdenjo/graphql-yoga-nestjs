8d1fe68d87eaf4ef5a3311c88d3ef56f73959aa6
Charly POLY <cpoly55@gmail.com>
2022-05-26 18:20:52 +0200

    moving implementation from https://github.com/nestjs/graphql

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  reportUnusedDisableDirectives: true,
+  env: {
+    node: true,
+  },
+  // TODO: remove this line to lint website folder from root folder
+  overrides: [
+    {
+      files: "*.{js,ts,jsx,tsx,cjs,cts,mjs,mts,cjsx,ctsx,mjsx,mtsx}",
+      parser: "@typescript-eslint/parser",
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+      ],
+      rules: {
+        "no-console": "error",
+      },
+    },
+  ],
+};
