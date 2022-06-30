a7129ec13048d67745ebde0e404d2bb3bea9cd79
Charly POLY <cpoly55@gmail.com>
2022-06-30 12:19:59 +0200

    prettier

--- scripts/canary-release.cjs
+++ scripts/canary-release.cjs
@@ -1,75 +1,76 @@
 /* eslint-disable */
-const semver = require('semver')
-const cp = require('child_process')
-const { basename, join } = require('path')
+const semver = require("semver");
+const cp = require("child_process");
+const { basename, join } = require("path");
 
-const { read: readConfig } = require('@changesets/config')
-const readChangesets = require('@changesets/read').default
-const assembleReleasePlan = require('@changesets/assemble-release-plan').default
-const applyReleasePlan = require('@changesets/apply-release-plan').default
-const { getPackages } = require('@manypkg/get-packages')
+const { read: readConfig } = require("@changesets/config");
+const readChangesets = require("@changesets/read").default;
+const assembleReleasePlan =
+  require("@changesets/assemble-release-plan").default;
+const applyReleasePlan = require("@changesets/apply-release-plan").default;
+const { getPackages } = require("@manypkg/get-packages");
 
 function getNewVersion(version, type) {
   const gitHash = cp
-    .spawnSync('git', ['rev-parse', '--short', 'HEAD'])
+    .spawnSync("git", ["rev-parse", "--short", "HEAD"])
     .stdout.toString()
-    .trim()
+    .trim();
 
-  return semver.inc(version, `pre${type}`, true, 'canary-' + gitHash)
+  return semver.inc(version, `pre${type}`, true, "canary-" + gitHash);
 }
 
 function getRelevantChangesets(baseBranch) {
   const comparePoint = cp
-    .spawnSync('git', ['merge-base', `origin/${baseBranch}`, 'HEAD'])
+    .spawnSync("git", ["merge-base", `origin/${baseBranch}`, "HEAD"])
     .stdout.toString()
-    .trim()
+    .trim();
   const listModifiedFiles = cp
-    .spawnSync('git', ['diff', '--name-only', comparePoint])
+    .spawnSync("git", ["diff", "--name-only", comparePoint])
     .stdout.toString()
     .trim()
-    .split('\n')
+    .split("\n");
 
   const items = listModifiedFiles
-    .filter((f) => f.startsWith('.changeset'))
-    .map((f) => basename(f, '.md'))
+    .filter((f) => f.startsWith(".changeset"))
+    .map((f) => basename(f, ".md"));
 
-  return items
+  return items;
 }
 
 async function updateVersions() {
-  const cwd = process.cwd()
+  const cwd = process.cwd();
   // Exit pre mode
   // await unlink(join(cwd, '.changeset/pre.json'))
-  const packages = await getPackages(cwd)
-  const config = await readConfig(cwd, packages)
-  const modifiedChangesets = getRelevantChangesets(config.baseBranch)
+  const packages = await getPackages(cwd);
+  const config = await readConfig(cwd, packages);
+  const modifiedChangesets = getRelevantChangesets(config.baseBranch);
   const changesets = (await readChangesets(cwd)).filter((change) =>
-    modifiedChangesets.includes(change.id),
-  )
+    modifiedChangesets.includes(change.id)
+  );
 
   if (changesets.length === 0) {
     console.warn(
-      `Unable to find any relevant package for canary publishing. Please make sure changesets exists!`,
-    )
-    process.exit(1)
+      `Unable to find any relevant package for canary publishing. Please make sure changesets exists!`
+    );
+    process.exit(1);
   } else {
     const releasePlan = assembleReleasePlan(
       changesets,
       packages,
       config,
       [],
-      false,
-    )
+      false
+    );
 
     if (releasePlan.releases.length === 0) {
       console.warn(
-        `Unable to find any relevant package for canary releasing. Please make sure changesets exists!`,
-      )
-      process.exit(1)
+        `Unable to find any relevant package for canary releasing. Please make sure changesets exists!`
+      );
+      process.exit(1);
     } else {
       for (const release of releasePlan.releases) {
-        if (release.type !== 'none') {
-          release.newVersion = getNewVersion(release.oldVersion, release.type)
+        if (release.type !== "none") {
+          release.newVersion = getNewVersion(release.oldVersion, release.type);
         }
       }
 
@@ -81,17 +82,17 @@ async function updateVersions() {
           commit: false,
         },
         false,
-        true,
-      )
+        true
+      );
     }
   }
 }
 
 updateVersions()
   .then(() => {
-    console.info(`Done!`)
+    console.info(`Done!`);
   })
   .catch((err) => {
-    console.error(err)
-    process.exit(1)
-  })
+    console.error(err);
+    process.exit(1);
+  });
