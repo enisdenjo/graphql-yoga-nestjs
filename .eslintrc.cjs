769906253ed290e396fde2cc1f7e1e4109280c74
enisdenjo <badurinadenis@gmail.com>
2023-01-25 13:47:50 +0100

    use shared-config and unpin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,3 @@
 module.exports = {
-  root: true,
-  reportUnusedDisableDirectives: true,
-  env: {
-    node: true,
-  },
-  // TODO: remove this line to lint website folder from root folder
-  overrides: [
-    {
-      files: "*.{js,ts,jsx,tsx,cjs,cts,mjs,mts,cjsx,ctsx,mjsx,mtsx}",
-      parser: "@typescript-eslint/parser",
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-      ],
-      rules: {
-        "@typescript-eslint/ban-types": 1,
-        "no-console": "error",
-      },
-    },
-  ],
+  extends: ["@theguild"],
 };
