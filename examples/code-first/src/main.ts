0f2f408dc4638157bf7052f51490b3f961b42880
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2023-02-10 03:06:40 +0300

    chore: Add a basic switch to cover both Fastify and Express in code-first example

--- examples/code-first/src/main.ts
+++ examples/code-first/src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+async function createFastifyApp() {
+  return NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+}
+
+async function createExpressApp() {
+  return NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter());
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = process.env.FASTIFY ? await createFastifyApp() : await createExpressApp();
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
