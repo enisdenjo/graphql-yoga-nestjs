14d56b3c5ec57c6e773a933b98c1037f1efbd43e
enisdenjo <badurinadenis@gmail.com>
2023-02-18 15:07:47 +0100

    unplugin-swc for testing nestjs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,10 @@
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({});
+export default defineConfig({
+  // esbuild does not fully support decorators and
+  // therefore cannot be used for testing nestjs
+  // https://github.com/nestjs/nest/issues/9228
+  // https://github.com/vitest-dev/vitest/issues/708
+  plugins: [swc.vite()],
+});
