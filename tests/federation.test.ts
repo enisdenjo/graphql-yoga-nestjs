64079d3fc52ff59ea836120db744b995415419b4
enisdenjo <badurinadenis@gmail.com>
2023-02-23 19:59:47 +0100

    test

--- tests/federation.test.ts
+++ tests/federation.test.ts
@@ -24,44 +24,44 @@ beforeAll(async () => {
     await apps.users.listen(0);
   }
 
-  // // posts subgraph
-  // {
-  //   const module = await Test.createTestingModule({
-  //     imports: [PostsModule],
-  //   }).compile();
-  //   apps.posts = module.createNestApplication();
-  //   await apps.posts.listen(0);
-  // }
+  // posts subgraph
+  {
+    const module = await Test.createTestingModule({
+      imports: [PostsModule],
+    }).compile();
+    apps.posts = module.createNestApplication();
+    await apps.posts.listen(0);
+  }
 
-  // // gateway
-  // const module = await Test.createTestingModule({
-  //   imports: [
-  //     GraphQLModule.forRoot<YogaGatewayDriverConfig>({
-  //       driver: YogaGatewayDriver,
-  //       gateway: {
-  //         supergraphSdl: new IntrospectAndCompose({
-  //           subgraphs: [
-  //             {
-  //               name: 'users',
-  //               url: (await apps.users.getUrl()) + '/graphql',
-  //             },
-  //             {
-  //               name: 'posts',
-  //               url: (await apps.posts.getUrl()) + '/graphql',
-  //             },
-  //           ],
-  //         }),
-  //       },
-  //     }),
-  //   ],
-  // }).compile();
-  // apps.gateway = module.createNestApplication();
-  // await apps.gateway.listen(0);
-  // apps.gatewayUrl = (await apps.gateway.getUrl()) + '/graphql';
+  // gateway
+  const module = await Test.createTestingModule({
+    imports: [
+      GraphQLModule.forRoot<YogaGatewayDriverConfig>({
+        driver: YogaGatewayDriver,
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: 'users',
+                url: (await apps.users.getUrl()) + '/graphql',
+              },
+              {
+                name: 'posts',
+                url: (await apps.posts.getUrl()) + '/graphql',
+              },
+            ],
+          }),
+        },
+      }),
+    ],
+  }).compile();
+  apps.gateway = module.createNestApplication();
+  await apps.gateway.listen(0);
+  apps.gatewayUrl = (await apps.gateway.getUrl()) + '/graphql';
 });
 
 afterAll(async () => {
   await Promise.all([apps.users?.close(), apps.posts?.close(), apps.gateway?.close()]);
 });
 
-it('should ', () => {});
+it.todo('should have a working gateway', () => {});
